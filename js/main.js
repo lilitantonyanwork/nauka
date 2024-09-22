4be23a5103ece88dd2f08090fd93157da89836d6
lilitantonyan <lilit@helix.am>
2024-09-22 22:57:29 +0400

    add responsive style

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function (){
     var swiper = new Swiper(".review-slider.swiper", {
         slidesPerView: 2,
         spaceBetween: 20,
+        allowTouchMove: true,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
@@ -9,7 +10,18 @@ $(function (){
         navigation: {
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev',
-                }
+                },
+        breakpoints: {
+            // mobile + tablet - 320-990
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 0
+            },
+            // desktop >= 991
+            991: {
+                slidesPerView: 2
+            }
+        }
     });
     var product_swiper = new Swiper(".swiper-slider", {
         slidesPerView: 1,
@@ -19,14 +31,20 @@ $(function (){
             clickable: true,
         }
     });
-
-        $('[data-toggle="tooltip"]').tooltip();
-    Fancybox.bind("[data-fancybox]", {
-        caption:false,
-        Thumbs: {
-            type: "classic",
-            axis: 'y'
-        },
+    //
+    //     $('[data-toggle="tooltip"]').tooltip();
+    // Fancybox.bind("[data-fancybox]", {
+    //     caption:false,
+    //     Thumbs: {
+    //         type: "classic",
+    //         axis: 'y'
+    //     },
+    // });
+    $('#sidebarCollapse').on('click', function () {
+        console.log('aaa')
+        $('#sidebar').toggleClass('active');
+        $('.sidebar-overlay').toggleClass('active');
+        $('body').toggleClass('hide-scrollbar');
     });
 
 })
